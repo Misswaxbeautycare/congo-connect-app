36ce9ff1fd1db6352d0c094ffd8cb1266ecfcdcd
Miss Nyunge | Data Analyst <missnyunge@gmail.com>
2026-07-21 13:01:00 +0200

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,23 +3,18 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class AuthService {
   static final SupabaseClient _client = Supabase.instance.client;
 
-  static Future<AuthResponse> signUp({
-    required String email,
-    required String password,
-  }) async {
-    return await _client.auth.signUp(
-      email: email,
-      password: password,
-    );
+  static Future<void> sendOtp(String email) async {
+    await _client.auth.signInWithOtp(email: email);
   }
 
-  static Future<AuthResponse> signIn({
+  static Future<AuthResponse> verifyOtp({
     required String email,
-    required String password,
+    required String token,
   }) async {
-    return await _client.auth.signInWithPassword(
+    return await _client.auth.verifyOTP(
+      type: OtpType.email,
       email: email,
-      password: password,
+      token: token,
     );
   }
 
